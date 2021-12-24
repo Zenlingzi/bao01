ef604c8cca37d5bb54b5bc212473bd1f0fedaf1e
Taiyang <91784694+Zenlingzi@users.noreply.github.com>
2021-12-24 21:07:40 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Cậu kia!',
+    introDesc: `Hôm nay là 1 ngày đặc biệt của cậu nên cậu hãy thật lòng nhé`,
+    btnIntro: 'OK',
+    title: 'Cậu có biết vì sao đầu tuần là thứ hai không :>',
+    desc: 'Hãy nghĩ kĩ nhé?? ',
+    btnYes: 'Kó',
+    btnNo: 'ko',
+    question:'Nói cho tớ lý do đi:>',
+    btnReply: 'Gửi cho tớ <3',
+    reply: 'Vì tớ là thứ nhất :3',
+    mess: 'Trời lạnh là tại đông. Yêu nàng là tại hạ :>',
+    messDesc: 'Tớ có món quà nhỏ muốn tặng cậu',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.youtube.com/watch?v=nvxF-dFNEKo'
+}
